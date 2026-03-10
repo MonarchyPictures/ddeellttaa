6c09421e7d15f5422f9a469084f8f84a9a4efb4f
MonarchyPictures <monarchyaudiovisual@gmail.com>
2026-03-10 00:36:45 -0400

    Add home endpoint to return status message

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def home():
+    return {"message": "Delta 9 running"}
+
 @app.get("/api/leads")
 async def get_leads():
     return {
